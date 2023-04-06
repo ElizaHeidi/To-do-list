d6c2ad794c6fb44a0e83042d38649fb5f51bc016
Elizabeth Heideman <77503008+ElizaHeidi@users.noreply.github.com>
2023-04-05 20:37:56 -0500

    Finalize functionality, begin styling

--- app.js
+++ app.js
@@ -5,15 +5,10 @@ let clearBtn = document.querySelector('.clear-task');
 let housekeepDiv = document.querySelector('.housekeep');
 const delAllBtn = document.querySelector('.delAll');
 const taskInput = document.querySelector('#task-input');
-
-// Load saved tasks from local storage
-if (localStorage.getItem('taskList')) {
-  ul.innerHTML = localStorage.getItem('taskList');
-}
-
 addBtn.addEventListener('click', getTask);
 clearBtn.addEventListener('click', clearTask);
 taskInput.addEventListener('click', stopTaskInput);
+ul.addEventListener('click', deleteTask); // Add event listener for click on the ul element
 
 let delAllBtnCreated = false;
 
@@ -39,37 +34,41 @@ function getTask() {
     icon.classList.add('fa-solid', 'fa-trash');
     trashBtn.appendChild(icon);
 
-    ul.appendChild(trashBtn);
+    // Create label and checkbox, populate text input 
+    li.appendChild(trashBtn);
 
-    ul.addEventListener('click', function(event) {
-      if (event.target.classList.contains('trash-icon')) {
-        console.log('Trash button clicked');
-        ul.removeChild(label);
-        ul.removeChild(checkbox);
-      }
-    });
-
-    // Create checkbox and label for task
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.name = 'checkbox';
     checkbox.value = li;
     checkbox.id = 'checkbox';
-    ul.appendChild(checkbox);
+    li.appendChild(checkbox);
 
     const label = document.createElement('label');
     label.textContent = document.getElementById('task-input').value;
-    ul.appendChild(label);
+    li.appendChild(label);
 
-    ul.appendChild(document.createElement('br'));
+    // Add li element to the ul element
+    ul.appendChild(li);
 
     // Save tasks to local storage
     localStorage.setItem('taskList', ul.innerHTML);
   }
-}
+} // getTask() ends
 
 function clearTask() {
   document.getElementById('task-input').value = '';
+}
+
+function deleteTask(e) {
+  if (e.target.classList.contains('trash-icon')) {
+    const li = e.target.parentNode;
+    const checkbox = li.querySelector('input[type="checkbox"]');
+    const label = li.querySelector('label');
+
+    ul.removeChild(li);
+    localStorage.setItem('taskList', ul.innerHTML);
+  }
 }
 
 function delAll() {
